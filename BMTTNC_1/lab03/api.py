6fe69e9d19d568e215159083f573c75bb9d22fce
nth064 <tienhuy2606@gmail.com>
2025-03-27 14:46:37 +0700

    lab04

--- BMTTNC_1/lab03/api.py
+++ BMTTNC_1/lab03/api.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, jsonify
 from cipher.rsa import RSACipher
+from cipher.ecc import ECCCipher
 
 app = Flask(__name__)
 
@@ -75,6 +76,52 @@ def rsa_verify_signature():
 
     return jsonify({'is_verified': is_verified})
 
+#-------
+
+ecc_cipher = ECCCipher()
+
+@app.route('/api/ecc/generate_keys', methods=['GET'])
+def ecc_generate_keys():
+    ecc_cipher.generate_keys()
+    return jsonify({'message': 'Keys generated successfully'})
+
+@app.route('/api/ecc/sign', methods=['POST'])
+def ecc_sign_message():
+    try:
+        data = request.get_json()
+        message = data.get('message')
+
+        if not message:
+            return jsonify({'error': 'Missing message'}), 400
+
+        private_key, _ = ecc_cipher.load_keys()  # Lấy khóa riêng tư
+        signature = ecc_cipher.sign(message, private_key)
+        signature_hex = signature.hex()
+
+        return jsonify({'signature': signature_hex})
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
+
+@app.route('/api/ecc/verify', methods=['POST'])
+def ecc_verify_signature():
+   
+    try:
+        data = request.get_json()
+        message = data.get('message')
+        signature_hex = data.get('signature')
+
+        if not message or not signature_hex:
+            return jsonify({'error': 'Missing message or signature'}), 400
+
+        public_key, _ = ecc_cipher.load_keys()  # Lấy khóa công khai
+        signature = bytes.fromhex(signature_hex)
+        is_verified = ecc_cipher.verify(message, signature, public_key)
+
+        return jsonify({'is_verified': is_verified})
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
+    
+    
 # Main function
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
