b51c1a173b7d9438db5e38b1103ef08ac28a2ef4
nth064 <tienhuy2606@gmail.com>
2025-03-27 16:20:07 +0700

    update

--- BMTTNC_1/lab04/hash/blake2.py
+++ BMTTNC_1/lab04/hash/blake2.py
@@ -1,18 +1,16 @@
-from Crypto.Hash import SHA3_256
+import hashlib
 
-def sha3(message):
-    """Tính toán giá trị hash SHA-3-256 từ dữ liệu đầu vào."""
-    sha3_hash = SHA3_256.new()
-    sha3_hash.update(message)
-    return sha3_hash.digest()
+def blake2(message):
+    blake2_hash = hashlib.blake2b(digest_size=64)
+    blake2_hash.update(message)
+    return blake2_hash.digest()
 
 def main():
-    """Nhận chuỗi đầu vào từ người dùng, mã hóa bằng SHA-3-256 và hiển thị kết quả."""
     text = input("Nhập chuỗi văn bản: ").encode('utf-8')
-    hashed_text = sha3(text)
+    hashed_text = blake2(text)
 
     print("Chuỗi văn bản đã nhập:", text.decode('utf-8'))
-    print("SHA-3 Hash:", hashed_text.hex())
+    print("BLAKE2 Hash:", hashed_text.hex())
 
 if __name__ == "__main__":
     main()
--- BMTTNC_1/lab04/hash/sha-3.py
+++ BMTTNC_1/lab04/hash/sha-3.py
@@ -1,13 +1,11 @@
 from Crypto.Hash import SHA3_256
 
 def sha3(message):
-    """Tính toán giá trị hash SHA-3-256 từ dữ liệu đầu vào."""
     sha3_hash = SHA3_256.new()
     sha3_hash.update(message)
     return sha3_hash.digest()
 
 def main():
-    """Nhận chuỗi đầu vào từ người dùng, mã hóa bằng SHA-3-256 và hiển thị kết quả."""
     text = input("Nhập chuỗi văn bản: ").encode('utf-8')
     hashed_text = sha3(text)
 
